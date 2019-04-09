4312b07b5b8b0b6646772a8156982f225fe5220b
Lance Jernigan <ljernigan@pyxl.com>
2019-04-08 20:36:10 -0400

    Setup webpack configs

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,37 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const workbox = require('workbox-webpack-plugin');
 
 const config = {
   entry: ['core-js/fn/promise', 'core-js/fn/array/find', 'whatwg-fetch', './src/index.js'],
   devtool: 'inline-source-map',
   mode: 'development',
   output: {
-    path: __dirname + '/dist',
-    filename: 'scripts/[name].js',
+    path: __dirname + '/build',
+    filename: 'js/site.js',
   },
   devServer: {
     publicPath: "/",
-    contentBase: './dist',
+    contentBase: './build',
     compress: true,
     port: 9000,
     historyApiFallback: true,
-  },
-  optimization: {
-      splitChunks: {
-          chunks: 'all',
-          maxInitialRequests: Infinity,
-          minSize: 0,
-          cacheGroups: {
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name(module) {
-                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-                return `npm.${packageName.replace('@', '')}`;
-              },
-            },
-          },
-      }
   },
   module: {
     rules: [
@@ -45,39 +29,33 @@ const config = {
         ],
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         include: /(src)/,
         use: [
-          'style-loader',
-          { 
-            loader: 'css-loader',
-            options: { 
-            modules: true,
-              localIdentName: '[name]__[local]___[hash:base64:5]',
-              importLoaders: 1,
-            },
+          {
+            loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('precss'),
-                ];
-              }
-            }
-          }
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
         ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader?name=/images/[name].[ext]'
+          'file-loader'
         ]
       }
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/site.css',
+      chunkFilename: '[id].css',
+    }),
     new CopyWebpackPlugin([
       {
         from: 'public'
@@ -86,12 +64,6 @@ const config = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
-    new workbox.GenerateSW({
-      swDest: 'service-worker.js',
-      clientsClaim: true,
-      skipWaiting: true,
-      runtimeCaching: []
-    })
   ],
 };
 
--- webpack.js
+++ webpack.js
@@ -1,31 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const workbox = require('workbox-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CompressionPlugin = require('compression-webpack-plugin');
 
 const config = {
   entry: ['core-js/fn/promise', 'core-js/fn/array/find', 'whatwg-fetch', './src/index.js'],
   mode: 'production',
   output: {
-    path: __dirname + '/dist',
-    filename: 'scripts/[name].js',
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
-    }
+    path: __dirname + '/build',
+    filename: 'assets/js/site.js',
   },
   module: {
     rules: [
@@ -39,53 +21,41 @@ const config = {
         ],
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         include: /(src)/,
         use: [
-          'style-loader',
-          'css-loader',
           {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('precss'),
-                ];
-              }
-            }
-          }
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
         ],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader?name=/images/[name].[ext]'
+          'file-loader?name=/assets/images/[name].[ext]'
         ]
       }
     ],
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: 'assets/css/site.css',
+      chunkFilename: '[id].css',
+    }),
     new CopyWebpackPlugin([
       {
         from: 'public'
       }
     ]),
     new HtmlWebpackPlugin({
-      template: 'public/index.html',
+      template: './public/index.html',
     }),
-    new workbox.GenerateSW({
-      swDest: 'service-worker.js',
-      clientsClaim: true,
-      skipWaiting: true,
-      runtimeCaching: [
-        {
-          urlPattern: 'http://i.imgur.com/**.{jpg,png}',
-          handler: 'cacheFirst',
-        },
-      ]
-    }),
-    new CompressionPlugin(),
   ],
 };
 
