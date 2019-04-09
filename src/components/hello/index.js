5785867549250d1148d6e50583d940cff1bff54b
Lance Jernigan <ljernigan@pyxl.com>
2019-04-08 21:36:38 -0400

    Update src

---
+++ src/components/hello/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from './style.scss';
+
+const Hello = () => (
+  <div
+    className="hello"
+  >
+    <h1>Hello from app</h1>
+  </div>
+);
+
+export default Hello;
---
+++ src/components/root/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Hello from '../hello';
+
+import style from './style.scss';
+
+const Root = () => (
+  <div
+    className="root"
+    id="root"
+  >
+    <Hello />
+  </div>
+);
+
+export default Root;
